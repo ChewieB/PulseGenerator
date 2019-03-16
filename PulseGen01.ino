4944ff9119347b7637a48b6db8dcc40a95233aac
Matt Baker <chewiebaker@gmail.com>
2019-03-16 10:09:13 +0000

    Working t1+t2 pulse gen

--- PulseGen01.ino
+++ PulseGen01.ino
@@ -6,7 +6,7 @@
 
 // D13 Pulse O/P
 
-
+#define D13 13
 
 void setup() {
   pinMode(13, OUTPUT);
@@ -15,7 +15,27 @@ void setup() {
 
 void loop() 
 {
+  // read pots to get pulse paramaters
   int pulseWidth = analogRead(A0);
   int pulseGap = analogRead(A1);
-  
+
+  // Set Pulse O/P Hi
+  digitalWrite(D13, HIGH);
+
+  // Calculate pulse width 1024/4 gives a range of 0-255
+  // 0 to 1/4 seconds can be checked by observation
+  pulseWidth /= 4;
+
+  // Calculate pulse gap this is going to be 3 times bigger for max values
+  // No good reason just going with it. Later will go for pulse frequency and width
+  pulseGap /= (3.0/4.0);
+
+  // Wait for the pulse width time
+  delay(pulseWidth);
+
+  // Set Pulse O/P lo
+  digitalWrite(D13, LOW);
+
+  // Wait for the pulse gap time
+  delay(pulseGap);
 }
