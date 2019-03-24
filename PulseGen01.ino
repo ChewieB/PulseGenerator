51f5dae48e13d3825866b152644223869318190a
Matt Baker <chewiebaker@gmail.com>
2019-03-24 14:11:59 +0000

    Switch to Freq and Width pot controls

--- PulseGen01.ino
+++ PulseGen01.ino
@@ -1,41 +1,53 @@
 //Pulse Generator 01
 // (c) Matt Baker 2019
-// Phase 1
-// A0 Pulse Width
-// A1 Pulse gap spacing
+// Phase 2
+// A0 frequency 10 -100 Hz
+// A1 Pulse Width 1 - 100 ms
 
 // D13 Pulse O/P
+// D12 Trigger O/P
 
-#define D13 13
+#define ouputPin 13
+#define triggerOutPin 12
 
 void setup() {
-  pinMode(13, OUTPUT);
-  
+  Serial.begin(9600);
+  pinMode(ouputPin, OUTPUT);
+  pinMode(triggerOutPin, OUTPUT);
 }
 
 void loop() 
 {
   // read pots to get pulse paramaters
-  int pulseWidth = analogRead(A0);
-  int pulseGap = analogRead(A1);
+  int frequencyIn = analogRead(A0);
+  int pulseWidthIn = analogRead(A1);
 
+  // Pulse the Trigger output Hi Lo for 3 us
+  digitalWrite(triggerOutPin, HIGH);
+  delayMicroseconds(3);
+  digitalWrite(triggerOutPin, LOW);
+  
   // Set Pulse O/P Hi
-  digitalWrite(D13, HIGH);
+  digitalWrite(ouputPin, HIGH);
 
-  // Calculate pulse width 1024/4 gives a range of 0-255
-  // 0 to 1/4 seconds can be checked by observation
-  pulseWidth /= 4;
-
-  // Calculate pulse gap this is going to be 3 times bigger for max values
-  // No good reason just going with it. Later will go for pulse frequency and width
-  pulseGap /= (3.0/4.0);
+  // Frequency calc
+  float frequency = 10.0 + (float)frequencyIn / 10.24;
+  
+  // Pulse period calc
+  float tTotal = 1.0 / frequency; // in seconds
+    
+  // Calculate pulse width 0-1023 1-11 ms
+  float pulseWidth = (((float)pulseWidthIn/100.0) * 1000.0) + 1000.0;
 
   // Wait for the pulse width time
-  delay(pulseWidth);
+  delayMicroseconds(pulseWidth);
 
-  // Set Pulse O/P lo
-  digitalWrite(D13, LOW);
-
-  // Wait for the pulse gap time
-  delay(pulseGap);
+  // Calculate Off time
+  float tOff = (tTotal * 1e6) - (pulseWidth);
+  if (tOff > 0)
+  {
+    delayMicroseconds(tOff);
+    // Set Pulse O/P lo
+    digitalWrite(ouputPin, LOW);
+  }
 }
